8a3302a93aaddeb4178f147365d7bb3bc30f0eff
Laurens Samson <laurenssamson@hotmail.com>
2020-05-18 15:42:13 +0200

    test

--- detect.py
+++ detect.py
@@ -96,7 +96,8 @@ def detect(original_image, min_score, max_overlap, top_k, suppress=None):
 
 
 if __name__ == '__main__':
-    img_path = '1d2eb3a0-8bda-43bc-bd92-11be497b217b.jpg'
+
+    img_path = "/content/gdrive/My Drive/Chess notation/annotated/1d2eb3a0-8bda-43bc-bd92-11be497b217b.jpg"
     original_image = Image.open(img_path, mode='r')
     original_image = original_image.convert('RGB')
     boxed_img = detect(original_image, min_score=0.2, max_overlap=0.5, top_k=200)
